b674441c26d6c97d44b2bf449a56b707bd894a96
Alexandre Fiori <fiorix@gmail.com>
2026-02-28 05:03:30 +0000

    Restore sun_path cast for portability
    The cast to *mut u8 is needed because libc::sockaddr_un.sun_path is
    [i8] on x86_64 glibc targets but [u8] on musl/aarch64.

--- src/proxy/client.rs
+++ src/proxy/client.rs
@@ -168,7 +168,7 @@ fn connect_unix(path: &str) -> Result<RawFd, Box<dyn std::error::Error>> {
     unsafe {
         std::ptr::copy_nonoverlapping(
             path_bytes.as_ptr(),
-            addr.sun_path.as_mut_ptr(),
+            addr.sun_path.as_mut_ptr() as *mut u8,
             path_bytes.len(),
         );
     }
